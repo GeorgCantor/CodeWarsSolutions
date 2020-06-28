f716441f4a62fa29d20443549fb449f73644331e
GeorgCantor <cupsman1986@gmail.com>
2020-06-28 15:49:41 +0300

    solved some tasks

--- app/src/main/java/com/example/codewarssolutions/KotlinActivity.kt
+++ app/src/main/java/com/example/codewarssolutions/KotlinActivity.kt
@@ -15,15 +15,146 @@ class KotlinActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Toast.makeText(
             this,
-            solution(10).toString(),
+            subtractSum(10),
             Toast.LENGTH_LONG
         ).show()
     }
 
-    fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int) = when (val result = dadYearsOld - (sonYearsOld * 2)) {
-        in 0..Int.MAX_VALUE -> result
-        else -> (sonYearsOld * 2) - dadYearsOld
+    fun subtractSum(n: Int): String {
+        var result = n
+        var sum = result
+
+        do {
+            result = get(sum)
+            sum = result
+        } while (result > 100)
+
+        return when (result) {
+            1 -> "kiwi"
+            2 -> "pear"
+            3 -> "kiwi"
+            4 -> "banana"
+            5 -> "melon"
+            6 -> "banana"
+            7 -> "melon"
+            8 -> "pineapple"
+            9 -> "apple"
+            10 -> "pineapple"
+            11 -> "cucumber"
+            12 -> "pineapple"
+            13 -> "cucumber"
+            14 -> "orange"
+            15 -> "grape"
+            16 -> "orange"
+            17 -> "grape"
+            18 -> "apple"
+            19 -> "grape"
+            20 -> "cherry"
+            21 -> "pear"
+            22 -> "cherry"
+            23 -> "pear"
+            24 -> "kiwi"
+            25 -> "banana"
+            26 -> "kiwi"
+            27 -> "apple"
+            28 -> "melon"
+            29 -> "banana"
+            30 -> "melon"
+            31 -> "pineapple"
+            32 -> "melon"
+            33 -> "pineapple"
+            34 -> "cucumber"
+            35 -> "orange"
+            36 -> "apple"
+            37 -> "orange"
+            38 -> "grape"
+            39 -> "orange"
+            40 -> "grape"
+            41 -> "cherry"
+            42 -> "pear"
+            43 -> "cherry"
+            44 -> "pear"
+            45 -> "apple"
+            46 -> "pear"
+            47 -> "kiwi"
+            48 -> "banana"
+            49 -> "kiwi"
+            50 -> "banana"
+            51 -> "melon"
+            52 -> "pineapple"
+            53 -> "melon"
+            54 -> "apple"
+            55 -> "cucumber"
+            56 -> "pineapple"
+            57 -> "cucumber"
+            58 -> "orange"
+            59 -> "cucumber"
+            60 -> "orange"
+            61 -> "grape"
+            62 -> "cherry"
+            63 -> "apple"
+            64 -> "cherry"
+            65 -> "pear"
+            66 -> "cherry"
+            67 -> "pear"
+            68 -> "kiwi"
+            69 -> "pear"
+            70 -> "kiwi"
+            71 -> "banana"
+            72 -> "apple"
+            73 -> "banana"
+            74 -> "melon"
+            75 -> "pineapple"
+            76 -> "melon"
+            77 -> "pineapple"
+            78 -> "cucumber"
+            79 -> "pineapple"
+            80 -> "cucumber"
+            81 -> "apple"
+            82 -> "grape"
+            83 -> "orange"
+            84 -> "grape"
+            85 -> "cherry"
+            86 -> "grape"
+            87 -> "cherry"
+            88 -> "pear"
+            89 -> "cherry"
+            90 -> "apple"
+            91 -> "kiwi"
+            92 -> "banana"
+            93 -> "kiwi"
+            94 -> "banana"
+            95 -> "melon"
+            96 -> "banana"
+            97 -> "melon"
+            98 -> "pineapple"
+            99 -> "apple"
+            100 -> "pineapple"
+            else -> ""
+        }
     }
+
+    private fun get(one: Int): Int {
+        val s = one.toString()
+        var sum = 0
+
+        s.map {
+            sum += it.toString().toInt()
+        }
+
+        return one - sum
+    }
+
+    fun divide(weight: Int) = weight % 2 == 0 && weight != 2
+
+    fun feast(beast: String, dish: String) =
+        beast.first() == dish.first() && beast.last() == dish.last()
+
+    fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int) =
+        when (val result = dadYearsOld - (sonYearsOld * 2)) {
+            in 0..Int.MAX_VALUE -> result
+            else -> (sonYearsOld * 2) - dadYearsOld
+        }
 
     // 7 kyu The Office I - Outed
     fun outed(
