75158b9812a5eb3e843caab533e62e81dcbf0b84
GeorgCantor <cupsman1986@gmail.com>
2021-01-15 16:52:15 +0300

    solved some tasks

--- app/src/main/java/com/example/codewarssolutions/leetcode/LeetCodeActivity.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/LeetCodeActivity.kt
@@ -10,6 +10,40 @@ class LeetCodeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leet_code)
 
-        val s = PhoneLetterSolution.letterCombinations("23")
+    }
+
+    fun containsDuplicate(nums: IntArray) = nums.size > nums.toSet().size
+
+    fun reverseString(s: CharArray): Unit {
+        var aPointer = 0
+        var bPointer = s.size - 1
+
+        while (aPointer <= bPointer) {
+            val temp = s[aPointer]
+            s[aPointer] = s[bPointer]
+            s[bPointer] = temp
+            aPointer++
+            bPointer--
+        }
+    }
+
+    fun isAnagram(s: String, t: String) =
+        s.toCharArray().sortedArray().contentEquals(t.toCharArray().sortedArray())
+
+    fun isAnagram2(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        val list = s.toMutableList()
+        t.forEach {
+            if (list.contains(it)) list.remove(it)
+        }
+
+        return list.isEmpty()
+    }
+
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val list = mutableSetOf<Int>()
+        nums1.forEach { if (nums2.contains(it)) list.add(it) }
+
+        return list.toIntArray()
     }
 }
