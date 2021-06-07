12eef3342e658d0adfef16fd6cb18bb3b4e22f17
GeorgCantor <cupsman1986@gmail.com>
2021-06-07 22:12:25 +0300

    Solve some LeetCode tasks

---
+++ app/src/main/java/com/example/codewarssolutions/leetcode2/KotlinActivity.kt
@@ -0,0 +1,31 @@
+package com.example.codewarssolutions.leetcode2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.codewarssolutions.R
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+
+    }
+
+    // https://leetcode.com/problems/maximum-69-number/
+    fun maximum69Number(n: Int) = n.toString().replaceFirst("6", "9").toInt()
+
+    // https://leetcode.com/problems/find-the-highest-altitude/
+    fun largestAltitude(ar: IntArray) = arrayOf(0, 0).apply {
+        ar.forEach { this[1] += it; this[0] = maxOf(first(), last()) }
+    }.first()
+
+    // https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
+    fun minOperations(ar: IntArray) = run {
+        var c = 0
+        for (i in 1 until ar.size) {
+            if (ar[i - 1] >= ar[i]) (ar[i - 1] - ar[i] + 1).apply { c += this; ar[i] += this }
+        }
+        c
+    }
+}
