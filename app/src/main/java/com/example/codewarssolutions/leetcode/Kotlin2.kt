14ac7a3c60d62132c4ffe5a4eb052bd75cce94fd
GeorgCantor <cupsmana16>
2022-04-20 19:51:35 +0300

    Solve some problems

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin2.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin2.kt
@@ -2,33 +2,6 @@ package com.example.codewarssolutions.leetcode
 
 import java.util.*
 
-// https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/
-fun displayTable(l: List<List<String>>): List<List<String>> {
-    val map = l.groupingBy { it.drop(1).joinToString(",") }.eachCount()
-    val header = mutableListOf("Table")
-    val lines = mutableListOf<MutableList<String>>()
-    l.sortedWith(compareBy<List<String>> { it.last() }.thenBy { it[1].toInt() }).forEach {
-        header.add(it.last())
-        lines.add(mutableListOf(it[1]))
-    }
-    val hD = header.distinct()
-    val lD = lines.distinct()
-    lD.forEach {
-        for (i in 1..hD.lastIndex) it.add(map.getOrDefault("${it[0]},${hD[i]}", 0).toString())
-    }
-    return mutableListOf<List<String>>().apply {
-        this.add(hD)
-        this.addAll(lD.sortedBy { it.first().toInt() })
-    }
-}
-
-// https://leetcode.com/problems/finding-the-users-active-minutes/
-fun findingUsersActiveMinutes(a: Array<IntArray>, k: Int) = IntArray(k).apply {
-    a.groupBy { it.first() }.mapValues { it.value.distinctBy { it.last() } }.entries.forEach {
-        ++this[it.value.lastIndex]
-    }
-}
-
 // https://leetcode.com/problems/sort-characters-by-frequency/
 fun frequencySort(s: String) =
     s.groupingBy { it }.eachCount().entries.sortedByDescending { it.value }.joinToString("") {
--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin3.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin3.kt
@@ -1,2 +1,39 @@
 package com.example.codewarssolutions.leetcode
 
+
+// https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/
+fun displayTable(l: List<List<String>>): List<List<String>> {
+    val map = l.groupingBy { it.drop(1).joinToString(",") }.eachCount()
+    val header = mutableListOf("Table")
+    val lines = mutableListOf<MutableList<String>>()
+    l.sortedWith(compareBy<List<String>> { it.last() }.thenBy { it[1].toInt() }).forEach {
+        header.add(it.last())
+        lines.add(mutableListOf(it[1]))
+    }
+    val hD = header.distinct()
+    val lD = lines.distinct()
+    lD.forEach {
+        for (i in 1..hD.lastIndex) it.add(map.getOrDefault("${it[0]},${hD[i]}", 0).toString())
+    }
+    return mutableListOf<List<String>>().apply {
+        this.add(hD)
+        this.addAll(lD.sortedBy { it.first().toInt() })
+    }
+}
+
+// https://leetcode.com/problems/finding-the-users-active-minutes/
+fun findingUsersActiveMinutes(a: Array<IntArray>, k: Int) = IntArray(k).apply {
+    a.groupBy { it.first() }.mapValues { it.value.distinctBy { it.last() } }.entries.forEach {
+        ++this[it.value.lastIndex]
+    }
+}
+
+// https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/
+fun cellsInRange(s: String) = mutableListOf<String>().apply {
+    val abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (i in abc.indexOf(s.first())..abc.indexOf(s[3])) {
+        for (j in Character.getNumericValue(s[1])..Character.getNumericValue(s.last())) {
+            add("${abc[i]}$j")
+        }
+    }
+}
