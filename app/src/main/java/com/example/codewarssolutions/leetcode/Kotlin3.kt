520ffe1f9993280327ec00768dd488fbd1b69f63
GeorgCantor <cupsman1986@gmail.com>
2023-04-30 14:42:00 +0300

    https://leetcode.com/problems/split-with-minimum-sum/

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin3.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin3.kt
@@ -202,6 +202,15 @@ fun digitSum(s: String, k: Int): String {
     return w
 }
 
+// https://leetcode.com/problems/split-with-minimum-sum/
+fun splitNum(num: Int): Int {
+    val pair = num.toString().toCharArray().sorted()
+        .foldIndexed(Pair(StringBuilder(), StringBuilder())) { i, p, c ->
+            if (i % 2 == 0) p.apply { first.append(c) } else p.apply { second.append(c) }
+        }
+    return pair.first.toString().toInt() + pair.second.toString().toInt()
+}
+
 // https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
 fun removeOccurrences(s: String, part: String): String {
     var w = s
