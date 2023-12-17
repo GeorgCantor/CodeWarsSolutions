1f1c6f96903c4279bad543b6ee356625b4368bed
GeorgCantor <cupsman1986@gmail.com>
2023-12-17 19:57:02 +0300

    https://leetcode.com/problems/card-flipping-game/ simplify

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
@@ -1,14 +1,6 @@
 package com.example.codewarssolutions.leetcode
 
-import kotlin.math.min
-
-
 // https://leetcode.com/problems/card-flipping-game/
 fun flipgame(f: IntArray, b: IntArray) = f.filterIndexed { i, n -> n == b[i] }.toSet().run {
-    var res = Int.MAX_VALUE
-    for (i in f.indices) {
-        if (f[i] !in this) res = min(res, f[i])
-        if (b[i] !in this) res = min(res, b[i])
-    }
-    if (res == Int.MAX_VALUE) 0 else res
+    f.plus(b).filter { it !in this }.minOrNull() ?: 0
 }
