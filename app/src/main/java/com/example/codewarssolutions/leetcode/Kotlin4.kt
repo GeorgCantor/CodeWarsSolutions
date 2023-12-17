1b7c4b13a2fc07d238ef817e21b522a224916fd4
GeorgCantor <cupsman1986@gmail.com>
2023-12-17 19:37:53 +0300

    https://leetcode.com/problems/design-browser-history/ set change

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
@@ -4,9 +4,8 @@ import kotlin.math.min
 
 
 // https://leetcode.com/problems/card-flipping-game/
-fun flipgame(f: IntArray, b: IntArray) = hashSetOf<Int>().run {
+fun flipgame(f: IntArray, b: IntArray) = f.filterIndexed { i, n -> n == b[i] }.toSet().run {
     var res = Int.MAX_VALUE
-    for (i in f.indices) if (f[i] == b[i]) add(f[i])
     for (i in f.indices) {
         if (f[i] !in this) res = min(res, f[i])
         if (b[i] !in this) res = min(res, b[i])
