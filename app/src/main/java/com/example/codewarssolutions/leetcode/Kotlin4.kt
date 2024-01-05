875f4f36c7c9010b058bc91c8e4bbf12a0872f35
GeorgCantor <cupsman1986@gmail.com>
2024-01-05 17:32:35 +0300

    https://leetcode.com/problems/subarrays-distinct-element-sum-of-squares-i/ simplify

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
@@ -4,3 +4,7 @@ package com.example.codewarssolutions.leetcode
 fun flipgame(f: IntArray, b: IntArray) = f.filterIndexed { i, n -> n == b[i] }.toSet().run {
     f.plus(b).filter { it !in this }.minOrNull() ?: 0
 }
+
+// https://leetcode.com/problems/subarrays-distinct-element-sum-of-squares-i/
+fun sumCounts(l: List<Int>) =
+    (1..l.size).sumOf { l.windowed(it).sumOf { val s = it.toSet().size; s * s } }
