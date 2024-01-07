9cf2dd92b6a393ead07a6a47f2339f3b7d12d040
GeorgCantor <cupsman1986@gmail.com>
2024-01-07 15:01:10 +0300

    https://leetcode.com/problems/count-tested-devices-after-test-operations/

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin4.kt
@@ -12,3 +12,12 @@ fun sumCounts(l: List<Int>) =
 // https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range/
 fun vowelStrings(a: Array<String>, l: Int, r: Int) =
     "aeiou".run { (l..r).count { a[it][0] in this && a[it].last() in this } }
+
+// https://leetcode.com/problems/count-tested-devices-after-test-operations/
+fun countTestedDevices(a: IntArray): Int {
+    var c = 0
+    for (i in a.indices) {
+        if (a[i] == 0) continue else for (j in i + 1..a.lastIndex) if (a[j] > 0) --a[j]; ++c
+    }
+    return c
+}
