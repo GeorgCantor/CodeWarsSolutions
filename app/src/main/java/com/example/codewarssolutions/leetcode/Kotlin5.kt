ea0f245e1ce143947ee6ab453b12aa1f424eb28f
GeorgCantor <cupsman1986@gmail.com>
2026-02-23 18:14:25 +0300

    https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/

--- app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin5.kt
+++ app/src/main/java/com/example/codewarssolutions/leetcode/Kotlin5.kt
@@ -8,3 +8,19 @@ fun possibleStringCount(s: String): Int {
     }
     return c
 }
+
+// https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/
+fun maxDifference(s: String): Int {
+    val a = IntArray(26)
+    s.forEach { a[it - 'a']++ }
+    var maxO = -1
+    var minE = Int.MAX_VALUE
+    for (n in a) {
+        when {
+            n == 0 -> continue
+            n % 2 == 0 -> if (n < minE) minE = n
+            else -> if (n > maxO) maxO = n
+        }
+    }
+    return maxO - minE
+}
